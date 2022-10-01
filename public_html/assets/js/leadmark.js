71f530e2f44cb056d36e8e1b0c30bc9e9544cdbf
Shubham Tiwary <shubhut17@gmail.com>
2022-10-01 15:08:08 +0530

    final site-1

--- public_html/assets/js/leadmark.js
+++ public_html/assets/js/leadmark.js
@@ -1,18 +1,3 @@
-//firebase config
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAwTMvEui9p0X1V_qtHxIPvyuFTWz083VE",
-    authDomain: "cognitio-2022.firebaseapp.com",
-    projectId: "cognitio-2022",
-    storageBucket: "cognitio-2022.appspot.com",
-    messagingSenderId: "481340855725",
-    appId: "1:481340855725:web:fb4b0f60059b484184fd24",
-    measurementId: "G-WT928RYLD5"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
 // smooth scroll
 $(document).ready(function(){
     $(".navbar .nav-link").on('click', function(event) {
@@ -54,55 +39,4 @@ $(window).on("load", function() {
             }
         }), !1
     })
-})
-
-//check if registration number is to disable payment
-
-const regnTag = document.querySelector("input[name='register']");
-const transId = document.querySelector("input[name='transId']");
-regnTag.addEventListener('change',(e)=>{
-    let regnValue = e.target.value;
-    let regex = /^2020UGME/i;
-    if(regex.test(regnValue))
-    {
-        transId.disabled = true;
-    }
-})
-
-let submitBtn = document.querySelector(".submit");
-submitBtn.addEventListener('click',async(e)=>{
-    e.preventDefault();
-   let sname = e.target.parentElement[name='name'].value;
-   let regnNo = e.target.parentElement[name='register'].value;
-   let email = e.target.parentElement[name='email'].value;
-   let batch = e.target.parentElement[name='batch'].value;
-   let transIdValue = transId.value;
-   let number = e.target.parentElement[name='number'].value;
-   let college = e.target.parentElement[name='college'].value;
-   let branch = e.target.parentElement[name='branch'].value;
-   if(sname===''||regnNo===''||email==''||number=='')
-   {
-      let p = document.createElement('p');
-      p.textContent = "Incomplete Fields";
-      p.style.color="red";
-      e.target.parentElement.insertBefore(p,e.target.parentElement.firstChild);
-      setTimeout(()=>{
-        p.remove();
-      },5000)
-       return;
-   }
-   let events = Array.from(e.target.parentElement.querySelectorAll('input[name=check]:checked')).map(e=>e.defaultValue);
-   let refUser = firebase.database().ref('registered_user');
-   let newRefUser = refUser.push();
-   newRefUser.set({
-    name:sname,
-    regnNo,
-    email,
-    batch,
-    transId,
-    college,
-    branch,
-    events
-
-   })
 })
